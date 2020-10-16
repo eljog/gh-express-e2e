e332fe11d0f25e7332dfc0b8cbe1204a0626f6bd
Burke Holland <burkeholland@Burkes-MBP.lan>
2020-10-16 16:37:59 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var products = require("../data/products.json");
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { products: products });
+});
+
+module.exports = router;
