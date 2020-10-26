e9e0389b9688d89c9e2ed8b6888477bf8b089b4a
Burke Holland <burkeholland@gmail.com>
2020-10-26 16:44:11 -0500

    Revert "Add fiveisprime route."

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,4 @@ router.get('/', function (req, res, next) {
   res.render('index', { products: products });
 });
 
-router.get('/fiveisprime', function (req, res, next) {
-  res.render('index', { products: [{
-    "name": "Single red garden gnome",
-    "price": 56,
-    "brand": {
-        "name": "Home & Pro tools"
-    },
-    "stockUnits": 98
-}] });
-});
-
 module.exports = router;
